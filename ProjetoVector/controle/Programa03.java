b3a5b010abde337535eac2458b294068c21ff712
Carlos Augusto <carlosaugusto20003@gmail.com>
2025-09-02 19:52:12 -0300

    Create Programa03.java

---
+++ ProjetoVector/controle/Programa03.java
@@ -0,0 +1,25 @@
+package controle;
+
+import java.util.Iterator;
+import java.util.Vector;
+
+import dominio.Animal;
+
+public class Programa03 {
+	public static void main(String[] args) {
+		Vector<Animal> animais = new Vector<Animal>();
+		
+		animais.add(new Animal("Betinha", "Canina", "Street Dog"));
+		animais.add(new Animal("Pachola", "Canina", "Street Dog"));
+		animais.add(new Animal("Mel", "Canina", "Poodle"));
+		animais.add(new Animal("Luana", "Canina", "Street Dog"));
+		animais.add(new Animal("Solange", "Canina", "Street Dog"));
+		
+		//System.out.println(animais);
+		Iterator iterator = animais.iterator();
+		while(iterator.hasNext()) {
+			Animal animal = (Animal)iterator.next();
+			System.out.println(animal.getNome() + " | " + animal.getEspecie() + " | " + animal.getRaca());
+		}
+	}
+}
